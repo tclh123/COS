37f7fcc36a7bbfa5b26e3e6647aa37e71c5df0b4
lh_intern <tclh123@gmail.com>
2013-12-30 14:13:53 +0800

    add databases/schema.sql & update README

---
+++ databases/schema.sql
@@ -0,0 +1,9 @@
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
