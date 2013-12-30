882d13108ad88d5dc930a94f5932cb058f4b6095
lh_intern <tclh123@gmail.com>
2013-12-30 18:54:54 +0800

    add user schema

--- databases/schema.sql
+++ databases/schema.sql
@@ -1,9 +1,53 @@
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user` (
+-- COS - Cafeteria Ordering System
+
+drop schema if exists `cosdb`;
+CREATE SCHEMA `cosdb`;
+use `cosdb`;
+
+/* clean up old tables;
+   must drop tables with foreign keys first
+   due to referential integrity constraints
+ */
+drop table if exists user;
+
+--
+-- Table structure for table "open_id"
+--
+CREATE TABLE `open_id_kind` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
+  `kind_name` varchar(20) NOT NULL,
   `created_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table "user"
+--
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `open_id_kind` int(11) NOT NULL,
+  `open_id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `type` varchar(16) NOT NULL,
+  `target_id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table "staff"
+--
+CREATE TABLE `staff` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `permissions` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table "permission"
+--
+CREATE TABLE `permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `desc` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
