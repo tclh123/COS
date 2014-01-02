db2a9f18566e163d88675cc591e7e273a4c0f598
lh_intern <tclh123@gmail.com>
2014-01-03 01:50:02 +0800

    add menu init data

--- databases/init_meal.sql
+++ databases/init_meal.sql
@@ -8,3 +8,6 @@ use `cosdb`;
 
 insert into meal values (NULL, '超好吃的菜菜', 2000, '超好吃超好吃哦', now(), now());
 insert into meal values (NULL, '超超好吃的菜菜', 2500, '超超好吃超好吃哦', now(), now());
+
+insert into menu values (NULL, 1, 2000, 50, 0, now(), now());
+insert into menu values (NULL, 2, 2350, 10, 0, now(), now());
