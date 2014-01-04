f383ed93821a50378b5741dfafe6131c94817992
lh_intern <tclh123@gmail.com>
2014-01-04 20:14:08 +0800

    add deliveryman user init

--- databases/init_user.sql
+++ databases/init_user.sql
@@ -11,8 +11,10 @@ use `cosdb`;
 insert into staff values (NULL, 7, now());
 insert into staff values (NULL, 3, now());
 insert into staff values (NULL, 1, now());
+insert into staff values (NULL, 4, now());
 
 insert into user values (NULL, 1, 13101249, 'Harry Lee', 'tclh123@gmail.com', 'user', NULL, now(), now());
 insert into user values (NULL, 1, 10000001, 'Ace', 'ace@tclh123.com', 'staff', 1, now(), now());
 insert into user values (NULL, 1, 10000002, 'KING', 'king@tclh123.com', 'staff', 2, now(), now());
 insert into user values (NULL, 1, 10000003, 'QUEEN', 'queen@tclh123.com', 'staff', 3, now(), now());
+insert into user values (NULL, 1, 10000004, 'DELIVERMAN', 'd@tclh123.com', 'staff', 4, now(), now());
